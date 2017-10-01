b2f1b23efe6857f46e1f4588bb28131f052e6775
Bigless27 <lessonryan@gmail.com>
2017-10-01 16:48:27 -0500

    first comment up to webpack.common setup

---
+++ src/vendor.ts
@@ -0,0 +1,12 @@
+// Angular
+import '@angular/common';
+import '@angular/core';
+import '@angular/http';
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/router';
+import 'rxjs';
+
+// RxJS
+// Other vendors for example jQuery, Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
