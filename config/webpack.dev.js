1eeff6b5c1cd05f9bcefe9dfb6af96c1a9bd6eec
Bigless27 <lessonryan@gmail.com>
2017-10-01 17:24:55 -0500

    add babel

---
+++ config/webpack.dev.js
@@ -0,0 +1,31 @@
+var webpackMerge = require('webpack-merge');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var commonConfig = require('./webpack.common.js');
+var helpers = require('./helpers');
+var webpack = require('webpack');
+
+module.exports = webpackMerge(commonConfig, {
+    devtool: 'cheap-module-eval-source-map',
+
+    output: {
+        path: helpers.root('dist'),
+        publicPath: '/',
+        filename: '[name].bundle.js',
+        chunkFilename: '[id].chunk.js'
+    },
+
+    devServer: {
+        historyApiFallback: true,
+        stats: 'minimal'
+    },
+
+    plugins: [
+        new ExtractTextPlugin('[name].css'),
+
+        new webpack.DefinePlugin({
+            'process.env': {
+                API_URL: JSON.stringify('http://localhost:5000/api/')
+            }
+        })
+    ]
+});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   constructor() {}
