3460f0fd54a8ee6cd2e92e6020428a3132ca90e9
Bigless27 <lessonryan@gmail.com>
2017-10-01 17:02:14 -0500

    add angular boiler-plate

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [CommonModule],
+  exports: [],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
